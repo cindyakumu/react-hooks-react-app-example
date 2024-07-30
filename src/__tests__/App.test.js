86db54cc6e8e37acf533d3157871537d0f858179
Cindy <cindyakumu40@gmail.com>
2024-07-30 09:11:25 +0300

    completed lab

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -1,6 +1,5 @@
 import "@testing-library/jest-dom";
 import { render, screen } from "@testing-library/react";
-import { format } from "date-fns";
 import App from "../components/App";
 
 beforeEach(() => {
@@ -8,9 +7,6 @@ beforeEach(() => {
 });
 
 test('should include "Now" in the header instead of a time', () => {
-  expect(
-    screen.queryByText(format(new Date(), "MMMM do yyyy, h:mm:ss a"))
-  ).not.toBeInTheDocument();
   expect(screen.queryByText(/Now/g)).toBeInTheDocument();
 });
 
@@ -34,4 +30,3 @@ test("should include the <TestComponent />", () => {
 //   it('should include the TestComponent', () => {
 //     expect(wrapper.text()).to.include('<TestComponent />')
 //   });
-// });
